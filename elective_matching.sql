6f1742be7a09f0eb8cb882e6849d0daf02e5793c
Toshali Goel <tg2@wellesley.edu>
2021-11-23 15:05:28 -0500

    adding new form for ading courses, fixing SQL bugs

--- elective_matching.sql
+++ elective_matching.sql
@@ -2,51 +2,24 @@ use electivematching_db;
 
 -- for refrential integrity add index 
 
-drop table if exists courses;
-drop table if exists professors;
--- drop table if exists students;
--- drop table if exists chooses;
 drop table if exists teaches;
+drop table if exists chooses;
+drop table if exists courses;
+drop table if exists user;
 
 create table user (
 	uid varchar(15) not null,
 	name varchar(30) not null,
-	primary key (uid)
-	year char(4),
-	userType enum('student', 'professor')
-	'''unsure if we want to do NUll as a string or NULL type'''
-	major1 enum('CS', 'DS', 'MAS', Null, 'other'),
+	primary key (uid),
+	classYear char(4),
+	userType enum('student', 'professor'),
+	major1 enum('CS', 'DS', 'MAS', 'other'),
     major2 varchar(30),
-	minor enum('CS', 'DS', 'MAS', Null, 'other'),
-	)
-
-'''
-create table students (
-	year char(4),
-	uid varchar not null,
-    major1 enum('CS', 'DS', 'MAS', Null, 'other'),
-    major2 varchar(30),
-    minor enum('CS', 'DS', 'MAS', Null, 'other'),
-	-- for refrential integrity
-	index(uid)
-    foreign key (uid) references users (uid)
-)
-
-
-
-Engine = InnoDB;
-
-
-create table professors (
-	uid varchar not null,
-	-- for refrential integrity
-	index(uid)
-	foreign key (uid) references users (uid)
+	minor enum('CS', 'DS', 'MAS', 'other'),
 	)
 
 Engine = InnoDB;
 
-'''
 
 create table courses (
 	courseid varchar(6) not null,
@@ -54,7 +27,7 @@ create table courses (
 	capacity tinyint not null,
 	waitlistCap tinyint,
 	primary key (courseid)
-	#Syllabus fileUpload change this to the correct type 
+	-- Syllabus fileUpload change this to the correct type 
 )
 
 Engine = InnoDB;
@@ -64,7 +37,7 @@ create table chooses (
 	course int not null,
 	rank int not null,
 	-- for refrential integrity
-	index(uid)
+	index(uid),
 	foreign key (user) references user(uid),
 	foreign key (course) references courses(courseid)
 	)
@@ -75,7 +48,7 @@ create table teaches (
 	professor varchar not null,
 	course int not null,
 	-- for refrential integrity
-	index(uid)
+	index(uid),
 	foreign key (user) references user(user),
 	foreign key (course) references courses(courseid)
 )
