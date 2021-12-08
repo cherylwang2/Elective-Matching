3baf42b3afda822852317792daa84596e97da4ce
mk1 <manasvi.khanna@wellesley.edu>
2021-12-08 16:03:26 -0500

    adding weighs query and prof query

--- course.py
+++ course.py
@@ -5,3 +5,31 @@ def viewCourses(conn):
     curs.execute('''select * from courses''')
     rows = curs.fetchall()
     return rows
+
+def courseGivenProf(conn, name ):
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+        select * from teaches   
+        where name = %s%''', [name])
+    return curs.fetchall()
+
+def courseGivenProf(conn, name ):
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+        select * from teaches   
+        where name = %s%''', [name])
+    return curs.fetchall()
+
+def courseGivenProf(conn, name ):
+    curs = dbi.dict_cursor(conn)
+    sql = ''' select * from teaches   
+        where name = %s%'''
+    curs.execute(sql, [name])
+    return curs.fetchall()
+
+def courseChosen(conn, rank):
+    curs = dbi.dict_cursor(conn)
+    sql = '''select * from chooses   
+        where courseRank = %s%'''
+    curs.execute(sql, [rank])
+    return curs.fetchall()
