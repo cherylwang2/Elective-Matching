178e32dd1478af8b253bf0508aa1f529e707f430
Cheryl Wang <cw5@wellesley.edu>
2021-12-11 17:23:42 -0500

    it's forcing me to

--- course.py
+++ course.py
@@ -1,7 +1,28 @@
 import cs304dbi as dbi
 
 def viewCourses(conn):
+    '''Selects all courses from the listed directory'''
     curs = dbi.dict_cursor(conn)
     curs.execute('''select * from courses''')
-    rows = curs.fetchall()
-    return rows
+    return curs.fetchall()
+
+def chooseCourse(conn, courseid):
+    '''Selects a course given its courseid'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''select * from courses where courseid = %s''',[courseid])
+    return curs.fetchone()
+
+def getStudents(conn, courseid):
+    '''selects all students registered in a course'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''select * from user where uid in (select uid from assignments where course=%s)''',[courseid])
+    return curs.fetchall()
+
+def insertCourse(conn, number, title, capacity, waitlistCap):
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''insert into courses (courseid, name, capacity, waitlistCap)
+                            values (%s, %s, %s, %s)''', [number, 
+                                                        title, 
+                                                        capacity, 
+                                                        waitlistCap])
+    conn.commit()
